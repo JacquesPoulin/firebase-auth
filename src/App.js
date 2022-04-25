4c51dfc05da88de7690a10a87ae91ed51d2402cc
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-25 20:40:58 +0200

    CRUD AUTH done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
 import ConnectModal from './components/ConnectModal';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from './utils/firebase.config';
+import CreatePost from './components/CreatePost';
 
 function App() {
 	const [user, setUser] = useState(null);
 
+	// >> manage the user Auth status
 	onAuthStateChanged(auth, (currentUser) => {
 		setUser(currentUser);
 	});
 
+	// >> manage the logout
+	const logOutHandler = async () => {
+		await signOut(auth);
+	};
+
 	return (
 		<div>
 			<div className='app-header'>
-				<ConnectModal />
+				{user && (
+					<div className='user-infos'>
+						<span>{user?.displayName[0]}</span>
+						<h4>{user?.displayName}</h4>
+						<button onClick={logOutHandler}>
+							<i className='fa-solid fa-arrow-right-from-bracket'></i>
+						</button>
+					</div>
+				)}
+
+				{user ? <CreatePost /> : <ConnectModal />}
 			</div>
 			<div className='post-container'></div>
 		</div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
+import { auth } from '../utils/firebase.config';
 
 const Login = () => {
-  return (
-    <div>
-      <h1>LOGIN</h1>
-    </div>
-  );
+	const loginEmail = useRef();
+	const loginPassword = useRef();
+
+	const [error, setError] = useState(false);
+
+	const loginHandler = async (e) => {
+		e.preventDefault();
+		try {
+			const user = await signInWithEmailAndPassword(
+				auth,
+				loginEmail.current.value,
+				loginPassword.current.value
+			);
+			console.log(user);
+		} catch (error) {
+			console.log(error.message);
+			setError(true);
+		}
+	};
+
+	return (
+		<div className='login-container'>
+			<div className='login'>
+				<h3>LOGIN</h3>
+				<form className='form-login' onSubmit={loginHandler}>
+					<input type='email' placeholder='Email' required ref={loginEmail} />
+					<input
+						type='password'
+						placeholder='Password'
+						required
+						ref={loginPassword}
+					/>
+					<input type='submit' value='login' />
+					<span>{error && 'Wrong ID or Password'}</span>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
